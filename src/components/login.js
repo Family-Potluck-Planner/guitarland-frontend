3c0153958adf480f01968108a549a3b5aa8dc1aa
Justin Peisker <justinpeisker@gmail.com>
2022-02-16 16:30:24 -0800

    wip

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialCreds = {
+    username: '',
+    password: '',
+}
 
 const Login = () => {
+    const [ creds, setCreds ] = useState(initialCreds)
+
+    const handleChange = (e) => {
+        setCreds({
+            ...creds,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        
+    }
+
     return(
         <div>
             <h1> Login </h1>
+            <form onSubmit = { handleSubmit }>
+                <label> Username 
+                    <input 
+                        name = 'username'
+                        placeholder = 'Enter username'
+                        onChange = { handleChange }
+                    />
+                </label>
+                <label> Password 
+                    <input  
+                        name = 'password'
+                        placeholder = 'Enter password'
+                        onChange = { handleChange }
+                    />
+                </label>
+                <button> Submit </button>
+            </form>
         </div>
     )
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialCreds = {
+    username: '',
+    password: '',
+}
 
 const Register = () => {
+    const [ creds, setCreds ] = useState(initialCreds)
+
+    const handleChange = (e) => {
+        setCreds({
+            ...creds,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return(
         <div>
             <h1> Register </h1>
+            <form onSubmit = {handleSubmit}>
+                <label> Username 
+                    <input 
+                        name = 'username'
+                        placeholder = 'Pick a username'
+                        onChange = {handleChange}
+                    />
+                </label>
+                <label> Password
+                    <input
+                        name = 'password'
+                        placeholder = 'Pick a password'
+                        onChange = {handleChange}
+                    />
+                </label>
+                <button> Submit </button>
+            </form>
         </div>
     )
 }
